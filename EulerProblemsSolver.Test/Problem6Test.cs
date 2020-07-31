bb96b988413edba51056765b9c4d1e1cac612632
UnsafeToSave <metro9393@mail.ru>
2020-07-31 17:11:31 +0300

    Added solution for problem 6

---
+++ EulerProblemsSolver.Test/Problem6Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using ProblemsSolver.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblemsSolver.Test
+{
+    [TestFixture]
+    class Problem6Test
+    {
+        private static IEnumerable<TestCaseData> InitialData
+        {
+            get
+            {
+                yield return new TestCaseData(2640, new Problem6(10));
+                yield return new TestCaseData(25164150, new Problem6(100));
+            }
+        }
+        [Test, TestCaseSource(nameof(InitialData))]
+        public void SolutionTest(long expectedResult, IProblem<long> problem)
+        {
+            //Act
+            problem.Solution();
+
+            //Assert
+            Assert.AreEqual(expectedResult, problem.GetResult());
+        }
+    }
+}
---
+++ ProblemsSolver/Problems/Problem6.cs
@@ -0,0 +1,40 @@
+﻿namespace ProblemsSolver.Problems
+{
+    // Problem description:
+    //The sum of the squares of the first ten natural numbers is,
+    //                       12 + 22 + ... + 102 = 385
+    //The square of the sum of the first ten natural numbers is,
+    //                    (1 + 2 + ... + 10)2 = 552 = 3025
+    //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
+    //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    public class Problem6 : IProblem<long>
+    {
+        int amountNumbers;
+        long difference;
+
+        public Problem6(int amount)
+        {
+            amountNumbers = amount;
+        }
+
+        public long GetResult()
+        {
+            return difference;
+        }
+
+        public void Solution()
+        {
+            long sumOfSquer = 0;
+            long squerOfSum = 0;
+
+            for (int i = 1; i <= amountNumbers; i++)
+            {
+                sumOfSquer += i * i;
+            }
+
+            squerOfSum = (amountNumbers * amountNumbers + amountNumbers) / 2;
+            squerOfSum = squerOfSum * squerOfSum;
+            difference = squerOfSum - sumOfSquer;
+        }
+    }
+}
